9600f6639dd3fa6a729c2176a4d96113a44dc2ad
ekim <ekim@nuvisionfederal.org>
2020-09-23 15:50:27 -0700

    Made changes as per Wendy request - Fee and Fee Amount in separate line

--- src/webparts/reactCalculator/components/ReactCalculator.tsx
+++ src/webparts/reactCalculator/components/ReactCalculator.tsx
@@ -74,28 +74,50 @@ export default class ReactCalculator extends React.Component<IReactCalculatorPro
               <span className={ styles.title }>{escape(this.props.description)}</span>
             
 
-              <TextField style={{color:"#00394b"}} label="Requested Amount" defaultValue="0" onBlur={this.setTwoDigits} onChange={this.handleChange} value={"$" + this.state.inputData.toString()}/>
               
-              <div style={{width: "100%", height: "auto" }}>
-                <p style={{padding: "0 1rem", color:"#00394b", display:"inline-block", fontSize: "1.5em"}}>
-                  Fee
-                </p> 
-                <p style={{borderStyle:"solid", borderRadius: "15px", padding: "0 1rem", borderColor: "#f5821f", color: "#00394b", fontSize: "1.5em", display: "inline-block", float: "right", fontWeight: "bold"}}>
-                                    
-                  {this.props.choice == "Add"? "+":""}
-                  {escape(this.props.inputRate.toString())} 
-                  {this.props.choice == "Multiply"? "x":""} 
-                  {this.props.choice == "Percent"? "%":""} 
-                </p>             
+              <div style={{width: "100%", height: "auto", display: "flex", flexDirection: "column" }}>
+                
+                <TextField style={{color:"#00394b"}} label="Requested Amount" defaultValue="0" onBlur={this.setTwoDigits} onChange={this.handleChange} value={"$" + this.state.inputData.toString()}/>
+                
+                <div style={{display: "flex" , flexDirection: "column" }}>
+
+      
+                  <div style={{margin: "1em 0" ,display: "flex" , flexDirection: "row", justifyContent:"space-between"}}>
+                    <p style={{margin: "0", padding: "0", color:"#00394b", fontSize: "1.5em"}}>
+                      Fee
+                    </p> 
+                    <p style={{margin: "0", borderStyle:"solid", borderRadius: "15px", padding: "0 1rem", borderColor: "#f5821f", color: "#00394b", fontSize: "1.5em", fontWeight: "bold"}}>                           
+                      {this.props.choice == "Add"? "+":""}
+                      {escape(this.props.inputRate.toString())} 
+                      {this.props.choice == "Multiply"? "x":""} 
+                      {this.props.choice == "Percent"? "%":""} 
+                    </p>   
+                  </div>
+      
+                  
+                  <div style={{margin: 0, display: "flex" , flexDirection: "row", justifyContent:"space-between"}}>
+                    <p style={{margin: 0, padding: "0", color:"#00394b", fontSize: "1.5em"}}>
+                      Fee Amount 
+                    </p> 
+
+              
+                    <p style={{margin: 0, borderStyle:"solid", borderRadius: "15px", padding: "0 1rem", borderColor: "navy", color: "#00394b", fontSize: "1.5em", fontWeight: "bold"}}>
+                      {this.props.choice == "Add"? "$" + this.props.inputRate: "" }
+                      {this.props.choice == "Multiply"? "N/A" :""} 
+                      {this.props.choice == "Percent"? "$" + (this.props.inputRate / 100 * this.state.inputData).toString():""} 
+                    </p>         
+
+
+                  </div> 
+
+                </div>   
+
+
               </div>
               
-              <div style={{width: "100%", height: "auto" }}>
-                <p style={{borderStyle:"solid", borderRadius: "15px", padding: "0 1rem", borderColor: "#f5821f", color: "#00394b", fontSize: "1.5em", display: "inline-block", float: "right", fontWeight: "bold"}}>
-                  {this.props.choice == "Add"? this.props.inputRate: "" }
-                  {this.props.choice == "Multiply"? this.props.inputRate :""} 
-                  {this.props.choice == "Percent"? (this.props.inputRate / 100 * this.state.inputData).toString():""} 
-                </p>
-              </div>
+            
+                
+     
 
               <TextField label="Total Amount " readOnly defaultValue="0" value={"$" + this.state.outputData.toFixed(2).toString()} />
               <DefaultButton style={{ backgroundColor: "#00394b", color: "white", margin: "1rem 0 0 0"}} text="Calculate" onClick={this.handleClick} allowDisabledFocus /> 
